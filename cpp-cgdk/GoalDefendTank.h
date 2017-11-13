f2505e6168e7ba16971ad56dc8562220b8b3dbc5
Victor Istomin <victor.send.mail.here@gmail.com>
2017-11-13 02:06:12 +0200

    don't rush with aircraft only

--- cpp-cgdk/GoalDefendTank.h
+++ cpp-cgdk/GoalDefendTank.h
@@ -5,14 +5,15 @@ namespace goals
 {
     class GoalDefendTank : public Goal
     {
-        static const int MAX_DEFEND_TICK = 10000;
+        static const int MAX_DEFEND_TICK            = 10000;
         static const int MAX_RESOLVE_CONFLICT_TICKS = 30;
-        static const int MAX_HEAL_TICKS = 1000;
-        
+        static const int MAX_HEAL_TICKS             = 1000;
+
         const double MIN_HEALTH_FACTOR = 0.03;
 
 
         const double m_helicopterIteration;  // size of movement emulation increment
+        const double m_maxAgressiveDistance;
         int          m_lastConflictTick = 0;
         int          m_lastAttackTick   = 0;
 
